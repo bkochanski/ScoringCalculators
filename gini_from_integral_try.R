4f45a42dde0ff7df8038bfc1800b5dd451b007d0
bkochanski <bkochanski@zie.pg.gda.pl>
2022-04-19 17:37:47 +0200

    calibrating gini_from_r function

--- gini_from_integral_try.R
+++ gini_from_integral_try.R
@@ -80,3 +80,66 @@ gini_from_r<-function(rho=.5, defrate=.1){
 }
 
 gini_from_r(.4,.1)
+
+
+
+# Error in integrate(function(x) { : maximum number of subdivisions reached 
+gini_combine_calculator(my_gini(df$default_flag, df$s1), my_gini(df$default_flag, 
+                                                                 df$s2), cor(df$s2, df$s1), mean(df$default_flag))
+
+gini_from_r<-function(rho=.5, defrate=.1){
+  F1<-function(s, d, rho){integrate(function(x){pnorm((qnorm(d)-rho*x)/sqrt(1-rho^2))*dnorm(x)}, lower=-Inf, upper=s)$value}
+  F2<-function(s,d,rho){pnorm((-qnorm(d)+rho*s)/sqrt(1-rho^2))*dnorm(s)}
+  F1_<-Vectorize(function(x){F1(x, defrate, rho)})
+  F2_<-Vectorize(function(x){F2(x, defrate, rho)})
+  2*integrate(function(x){F1_(x)*F2_(x)}, 
+              lower=-Inf, upper=Inf)$value/defrate/(1-defrate)-1
+}
+
+gini_combine_calculator(0.3241679, 0.292137, 0.01201619, 0.04160888)
+#?integrate
+
+gini_from_r<-function(rho=.5, defrate=.1){
+  F1<-function(s, d, rho){integrate(function(x){pnorm((qnorm(d)-rho*x)/sqrt(1-rho^2))*dnorm(x)}, lower=-Inf, upper=s)$value}
+  F2<-function(s,d,rho){pnorm((-qnorm(d)+rho*s)/sqrt(1-rho^2))*dnorm(s)}
+  F1_<-Vectorize(function(x){F1(x, defrate, rho)})
+  F2_<-Vectorize(function(x){F2(x, defrate, rho)})
+  2*integrate(function(x){F1_(x)*F2_(x)}, 
+              lower=-Inf, upper=Inf, subdivisions=200)$value/defrate/(1-defrate)-1
+}
+
+gini_combine_calculator(0.3241679, 0.292137, 0.01201619, 0.04160888)
+
+
+gini_from_r<-function(rho=.5, defrate=.1){
+  F1<-function(s, d, rho){integrate(function(x){pnorm((qnorm(d)-rho*x)/sqrt(1-rho^2))*dnorm(x)}, lower=-Inf, upper=s)$value}
+  F2<-function(s,d,rho){pnorm((-qnorm(d)+rho*s)/sqrt(1-rho^2))*dnorm(s)}
+  F1_<-Vectorize(function(x){F1(x, defrate, rho)})
+  F2_<-Vectorize(function(x){F2(x, defrate, rho)})
+  2*integrate(function(x){F1_(x)*F2_(x)}, 
+              lower=-Inf, upper=Inf, subdivisions=500)$value/defrate/(1-defrate)-1
+}
+
+r_from_gini(.284, .1677)
+
+gini_from_r<-function(rho=.5, defrate=.1){
+  F1<-function(s, d, rho){integrate(function(x){pnorm((qnorm(d)-rho*x)/sqrt(1-rho^2))*dnorm(x)}, lower=-Inf, upper=s)$value}
+  F2<-function(s,d,rho){pnorm((-qnorm(d)+rho*s)/sqrt(1-rho^2))*dnorm(s)}
+  F1_<-Vectorize(function(x){F1(x, defrate, rho)})
+  F2_<-Vectorize(function(x){F2(x, defrate, rho)})
+  2*integrate(function(x){F1_(x)*F2_(x)}, 
+              lower=-Inf, upper=Inf, subdivisions=500, rel.tol = .Machine$double.eps^.25)$value/defrate/(1-defrate)-1
+}
+
+gini_crd_test<-function(rho=0.5, defrate=0.1, gran=10000) {
+  #function translating rho into gini for a given defrate
+  drates_i<-pnorm(qnorm(defrate), rho*(qnorm(1:(gran-1)/gran)),sqrt(1-rho^2))
+  drates_2i<-(c(1, drates_i)+c(drates_i, 0))/2
+  return(ginic(cumsum((drates_2i)/sum(drates_2i)), cumsum((1-drates_2i)/sum(1-drates_2i))))
+}
+
+gini_from_r()
+gini_crd_test(gran=100000000)
+
+gini_combine_calculator(0.4517029, 0.450631, 0.3589866, 0.2633053) 
+
--- simulation2_psycho3.R
+++ simulation2_psycho3.R
@@ -1,5 +1,5 @@
-#data<-readxl::read_excel('C:/Users/Błażej/Downloads/data.xlsx')
-#data<-data[-c(1),]
+data<-readxl::read_excel('C:/Users/Błażej/Downloads/data.xlsx')
+data<-data[-c(1),]
 ginic<-function(bc, gc){
   #function for gini when we have cumulative goods and bads vectors
   sum((gc[2:(length(gc))]-gc[1:(length(gc)-1)])*
@@ -12,7 +12,7 @@ gini_from_r<-function(rho=.5, defrate=.1){
   F1_<-Vectorize(function(x){F1(x, defrate, rho)})
   F2_<-Vectorize(function(x){F2(x, defrate, rho)})
   2*integrate(function(x){F1_(x)*F2_(x)}, 
-              lower=-Inf, upper=Inf)$value/defrate/(1-defrate)-1
+              lower=-Inf, upper=Inf, subdivisions=500, rel.tol = .Machine$double.eps^.24)$value/defrate/(1-defrate)-1
 }
 
 gini_combine_calculator<-function(g1, g2, corr, defaultrate){
@@ -28,10 +28,16 @@ gini_combine_calculator<-function(g1, g2, corr, defaultrate){
   g_result0<-gini_from_r(corr_opt, defaultrate)
   g_result1<-if(a_opt<0 | a_opt>1000) {NaN} else {g_result0}
   return(c(new_gini=g_result1, 
-           #new_gini_2=g_result0, 
            a_opt=a_opt, score_1_weight=a_opt/(1+a_opt), score_2_weight=1/(1+a_opt), 
-           rho1=rho_s1, rho2=rho_s2, new_corr=corr_opt))
+           rho1=rho_s1, rho2=rho_s2, new_corr=corr_opt, gini1=g1, gini2=g2, corr=corr, defrate=defaultrate))
 }
+
+my_gini<-function(resp, pred){
+  c<-pred[order(pred)]
+  d<-resp[order(pred)]
+  bc<-c(0, cumsum(d)/sum(d))
+  gc<-c(0, cumsum(1-d)/sum(1-d))
+  sum((gc[2:(length(gc))]-gc[1:(length(gc)-1)])*(bc[2:(length(bc))]+bc[1:(length(bc)-1)]))-1} 
 
 
 gini_from_auc<-function(x){(x-.5)*2}
@@ -48,23 +54,26 @@ data$Gini_Combined<-gini_from_auc(data$AUC_Combined)
 gini_predict<-function(g1, g2, corr, defaultrate){unname(gini_combine_calculator(g1, g2, corr, defaultrate)[1])}
 gini_predict<-Vectorize(gini_predict)
 
+data$Gini_Predicted<-gini_predict(data$Gini_Bureau, data$Gini_Psych,
+                                  data$r_Bureau_Psych, data$Bad_Rate)
+
 
 r_from_gini<-function(gini, defaultrate=.1){
   phi_s1<-function(x){gini_from_r(rho=x, defrate=defaultrate)-gini}
   uniroot(phi_s1,lower=0,upper=1,tol = .Machine$double.eps)$root
 }
 
-for (j in 1:1){
+tot_results<-list()
+
+for (j in 1:5){
   print(j)
   n=data$N[j]
-  sigma <- matrix(c(1, data$r_Bureau_Psych[j], r_from_gini(gini_from_auc(data$AUC_Bureau[j]), data$Bad_Rate[j]),
-                    data$r_Bureau_Psych[j], 1, r_from_gini(gini_from_auc(data$AUC_Psych[j]), data$Bad_Rate[j]),
-                    r_from_gini(gini_from_auc(data$AUC_Bureau[j]), data$Bad_Rate[j]), r_from_gini(gini_from_auc(data$AUC_Psych[j]), data$Bad_Rate[j]), 1), 
+  dfrate=data$Bad_Rate[j]
+  sigma <- matrix(c(1, data$r_Bureau_Psych[j], r_from_gini(gini_from_auc(data$AUC_Bureau[j]), dfrate),
+                    data$r_Bureau_Psych[j], 1, r_from_gini(gini_from_auc(data$AUC_Psych[j]), dfrate),
+                    r_from_gini(gini_from_auc(data$AUC_Bureau[j]), dfrate), r_from_gini(gini_from_auc(data$AUC_Psych[j]), dfrate), 1), 
                       nrow=3)
   
-  }
-
-
 #auc_from_gini(gini_from_r(sigma[1,3], defrate = data$Bad_Rate[1]))
 #r_from_gini(gini_from_auc(data$AUC_Bureau[1]), data$Bad_Rate[1])
 
@@ -80,5 +89,50 @@ for (i in 1:100){
   #cor(df$s1, df$s2)
   #mean(df$default_flag)
   res1<-gini_combine_calculator(my_gini(df$default_flag, df$s1), my_gini(df$default_flag, df$s2), cor(df$s2, df$s1), mean(df$default_flag))
+  mylogit <- glm(I(1-default_flag) ~ s1 + s2, data = df, family = "binomial")
+  logit1_a<-mylogit$coefficients[2]/mylogit$coefficients[3]
+  gini_logit1<-my_gini(df$default_flag, logit1_a*df$s1+df$s2)
+  tot_results[[length(tot_results)+1]]<-c(j=j, 
+                                          i=i, 
+                                          n=n, 
+                                          res1,
+                                          gini_logit1=gini_logit1)
+}}
   
-}
+bind_tot_results<-do.call("rbind", tot_results)
+df_tot_results<-data.frame(matrix(unlist(bind_tot_results), ncol=length(tot_results[[1]])))
+names(df_tot_results)<-names(tot_results[[1]])
+df_tot_results$gini1_increase<-df_tot_results$new_gini-df_tot_results$gini1
+df_tot_results$gini2_increase<-df_tot_results$new_gini-df_tot_results$gini2
+df_tot_results$gini1_increl<-df_tot_results$new_gini/df_tot_results$gini1-1
+df_tot_results$gini2_increl<-df_tot_results$new_gini/df_tot_results$gini2-1
+
+#View(df_tot_results)
+sd((df_tot_results$gini1_increase)[df_tot_results$j==4])
+aggregate(df_tot_results$gini1_increase, sd, by=list(df_tot_results$j))
+aggregate(df_tot_results$gini2_increase, sd, by=list(df_tot_results$j))
+aggregate(df_tot_results$gini1_increl, sd, by=list(df_tot_results$j))
+aggregate(df_tot_results$gini2_increl, sd, by=list(df_tot_results$j))
+
+aggregate(df_tot_results$gini1, mean, by=list(df_tot_results$j))
+data$Gini_Bureau
+aggregate(df_tot_results$gini2, mean, by=list(df_tot_results$j))
+data$Gini_Psych
+aggregate(df_tot_results$new_gini, mean, by=list(df_tot_results$j))
+aggregate(df_tot_results$new_gini, sd, by=list(df_tot_results$j))
+aggregate(df_tot_results$gini1_increase, sd, by=list(df_tot_results$j))
+data$Gini_Predicted
+
+# for (i in 201:210){
+# print(gini_predict(df_tot_results$gini1[i], df_tot_results$gini2[i],
+#              df_tot_results$corr[i], df_tot_results$defrate[i])-df_tot_results$new_gini[i])
+# }
+aggregate((df_tot_results$gini_logit1-df_tot_results$new_gini), mean, by=list(df_tot_results$j))
+aggregate((df_tot_results$gini_logit1-df_tot_results$new_gini), median, by=list(df_tot_results$j))
+aggregate((df_tot_results$gini_logit1-df_tot_results$new_gini), sd, by=list(df_tot_results$j))
+hist(I(df_tot_results$gini_logit1-df_tot_results$new_gini)[df_tot_results$j==3], breaks=30)
+boxplot(I(df_tot_results$gini_logit1-df_tot_results$new_gini)~df_tot_results$j)
+data$N
+data$r_Bureau_Psych
+
+df_tot_results[df_tot_results$j==1,]
